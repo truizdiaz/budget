094e6f4e5c6df09adf255c4159a50409da81b203
Tomas Ruiz Diaz <external.diaz_tomas-ruiz@allianzdirect.de>
2024-04-21 12:56:03 +0200

    add expense

--- src/app/components/expenses/add-expense/add-expense.component.ts
+++ src/app/components/expenses/add-expense/add-expense.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Expense } from '../../../interfaces/expense';
 
 @Component({
   selector: 'app-add-expense',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './add-expense.component.scss'
 })
 export class AddExpenseComponent {
+  name: string = '';
+  amount: number = 0;
 
+  formInvalid: boolean = false;
+
+  addExpense() {
+    // Validations
+    if (this.name === '' || this.amount <= 0) {
+      this.formInvalid = true
+    } else {
+      // Create the object
+      const expense: Expense = {
+        name: this.name,
+        amount: this.amount
+      }
+
+      console.log(expense)
+
+      // Send data using Observables
+
+      // Reset the form
+      this.formInvalid = false;
+      this.name = '';
+      this.amount = 0;
+    }
+
+
+  }
 }
