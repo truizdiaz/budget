9c891a83091d9aadd2bc53e07cfdc761c3c0cdce
Tomas Ruiz Diaz <external.diaz_tomas-ruiz@allianzdirect.de>
2024-04-21 10:54:18 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { EnterBudgetComponent } from './components/enter-budget/enter-budget.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'enter-budget', pathMatch: 'full'},
+  { path: 'enter-budget', component: EnterBudgetComponent },
+  { path: 'expenses', component: ExpensesComponent },
+  { path: '**', redirectTo: 'enter-budget', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
